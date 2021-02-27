4d8105b27f962b333561b23c1b4739d073bd93c0
macea <maceascai@gmail.com>
2021-02-27 14:20:54 +0200

    clase zoo si zookeeper

---
+++ seminar1-CTS/src/clase/Animal.java
@@ -0,0 +1,24 @@
+package clase;
+
+public class Animal {
+	private String nume;
+public Animal(String nume) {
+		super();
+		this.nume = nume;
+	}
+
+
+public String getNume() {
+	return nume;
+}
+
+public void setNume(String nume) {
+	this.nume = nume;
+}	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
