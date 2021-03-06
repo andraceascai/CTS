c64754a6694dfdcb292466449e29a4d99537d7d2
macea <maceascai@gmail.com>
2021-03-06 15:12:47 +0200

    transforming interface into abstract class

--- seminar2-CTS/src/ro/ase/cts/programe/Program.java
+++ seminar2-CTS/src/ro/ase/cts/programe/Program.java
@@ -4,16 +4,21 @@ package ro.ase.cts.programe;
 import java.io.FileNotFoundException;
 import java.util.List;
 import ro.ase.cts.clase.readers.ReaderAngajat;
+import ro.ase.cts.clase.readers.ReaderAplicanti;
 import ro.ase.cts.clase.readers.ReaderElev;
 import ro.ase.cts.clase.Angajat;
 import ro.ase.cts.clase.Aplicant;
 
 public class Program {
 
+	public static List<Aplicant> citesteAplicanti(ReaderAplicanti reader) throws FileNotFoundException{
+		return reader.readAplicanti();
+	}
+	
 	public static void main(String[] args) {
 		List<Aplicant> listaAplicanti;
 		try {
-			listaAplicanti = ReaderElev.readPupil("elevi.txt");
+			listaAplicanti = citesteAplicanti(new ReaderAngajat("angajati.txt"));
 			for(Aplicant aplicant:listaAplicanti)
 				System.out.println(aplicant.toString());
 		} catch (FileNotFoundException e) {
